40da039cd9722e82c3e10474fc279356598ca7c7
inyong <jeong4726@gmail.com>
2022-06-21 14:33:16 +0900

    test: commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -329,7 +329,7 @@ function Home() {
     },
     onChange(info) {
       if (info.file.status !== "uploading") {
-        console.log(info.file, info.fileList);
+        // console.log(info.file, info.fileList);
       }
       if (info.file.status === "done") {
         message.success(`${info.file.name} file uploaded successfully`);
