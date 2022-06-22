9c59b32065e837d0d4da14d013a854f0cf7908d8
inyong <jeong4726@gmail.com>
2022-06-22 17:34:06 +0900

    commit : commit

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -11,7 +11,7 @@
 */
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
-import { USER_SERVER } from '../components/config';
+import { USER_SERVER } from "../components/config";
 
 import {
   Layout,
@@ -117,189 +117,186 @@ const signin = [
     />
   </svg>,
 ];
-export default  function SignIn(){
-  
+export default function SignIn() {
   const history = useHistory();
   const onFinish = (values) => {
-    axios.post(`${USER_SERVER}/login`, values)
-      .then(res => {
-        if (res.data) {
-          console.log(res)
-          document.cookie = `jwt=${res.data}`;
-          history.push('/dashboard');
-        } else {
-          window.alert(res.data)
+    axios.post(`${USER_SERVER}/login`, values).then((res) => {
+      if (res.data) {
+        console.log(res);
+        document.cookie = `jwt=${res.data}`;
+        history.push("/dashboard");
+      } else {
+        window.alert(res.data);
       }
-    })
-      console.log("Success:", values);
-    };
+    });
+    // console.log("Success:", values);
+  };
 
-    const onFinishFailed = (errorInfo) => {
-      console.log("Failed:", errorInfo);
-    };
-    return (
-      <>
-        <Layout className="layout-default layout-signin">
-          <Header>
-            <div className="header-col header-brand">
-              <h5>And Then Some 대시보드</h5>
-            </div>
-            <div className="header-col header-nav">
-              <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
-                <Menu.Item key="1">
-                  <Link to="/dashboard">
-                    {template}
-                    <span> 대시보드</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Link to="/profile">
-                    {profile}
-                    <span>프로필</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Link to="/sign-up">
-                    {signup}
-                    <span> 회원가입</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="4">
-                  <Link to="/sign-in">
-                    {signin}
-                    <span> 로그인</span>
-                  </Link>
-                </Menu.Item>
-              </Menu>
-            </div>
-            <div className="header-col header-btn">
-              <Button type="primary">무료 다운로드</Button>
-            </div>
-          </Header>
-          <Content className="signin">
-            <Row gutter={[24, 0]} justify="space-around">
-              <Col
-                xs={{ span: 24, offset: 0 }}
-                lg={{ span: 6, offset: 2 }}
-                md={{ span: 12 }}
-              >
-                <Title className="mb-15">로그인</Title>
-                <Title className="font-regular text-muted" level={5}>
-                  이메일과 비밀번호를 입력하세요.
-                </Title>
-                <Form
-                  onFinish={onFinish}
-                  onFinishFailed={onFinishFailed}
-                  layout="vertical"
-                  className="row-col"
-                >
-                  <Form.Item
-                    className="username"
-                    label="이메일"
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "이메일을 입력하세요!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="이메일" />
-                  </Form.Item>
-
-                  <Form.Item
-                    className="username"
-                    label="비밀번호"
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: "비밀번호를 입력하세요!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="비밀번호" type='password' />
-                  </Form.Item>
-
-                  <Form.Item
-                    // name="remember"
-                    className="aligin-center"
-                    valuePropName="checked"
-                  >
-                    <Switch defaultChecked onChange={onChange} />
-                    로그인 유지
-                  </Form.Item>
-
-                  <Form.Item>
-                    <Button
-                      type="primary"
-                      htmlType="submit"
-                      style={{ width: "100%" }}
-                    >
-                      로그인
-                    </Button>
-                  </Form.Item>
-                  <p className="font-semibold text-muted">
-                    계정이 없으신가요?&nbsp;{" "}
-                    <Link to="/sign-up" className="text-dark font-bold">
-                      회원가입
-                    </Link>
-                  </p>
-                </Form>
-              </Col>
-              <Col
-                className="sign-img"
-                style={{ padding: 12 }}
-                xs={{ span: 24 }}
-                lg={{ span: 12 }}
-                md={{ span: 12 }}
-              >
-                <img src={signinbg} alt="" />
-              </Col>
-            </Row>
-          </Content>
-          <Footer>
-            <Menu mode="horizontal">
-              <Menu.Item>회사</Menu.Item>
-              <Menu.Item>가치</Menu.Item>
-              <Menu.Item>팀소개</Menu.Item>
-              <Menu.Item>제품</Menu.Item>
-              <Menu.Item>블로그</Menu.Item>
-              <Menu.Item>가격정책</Menu.Item>
-            </Menu>
-            <Menu mode="horizontal" className="menu-nav-social">
-              <Menu.Item>
-                <Link to="#">{<DribbbleOutlined />}</Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to="#">{<TwitterOutlined />}</Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to="#">{<InstagramOutlined />}</Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to="#">
-                  <svg
-                    width="18"
-                    height="18"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M496 256c0 137-111 248-248 248-25.6 0-50.2-3.9-73.4-11.1 10.1-16.5 25.2-43.5 30.8-65 3-11.6 15.4-59 15.4-59 8.1 15.4 31.7 28.5 56.8 28.5 74.8 0 128.7-68.8 128.7-154.3 0-81.9-66.9-143.2-152.9-143.2-107 0-163.9 71.8-163.9 150.1 0 36.4 19.4 81.7 50.3 96.1 4.7 2.2 7.2 1.2 8.3-3.3.8-3.4 5-20.3 6.9-28.1.6-2.5.3-4.7-1.7-7.1-10.1-12.5-18.3-35.3-18.3-56.6 0-54.7 41.4-107.6 112-107.6 60.9 0 103.6 41.5 103.6 100.9 0 67.1-33.9 113.6-78 113.6-24.3 0-42.6-20.1-36.7-44.8 7-29.5 20.5-61.3 20.5-82.6 0-19-10.2-34.9-31.4-34.9-24.9 0-44.9 25.7-44.9 60.2 0 22 7.4 36.8 7.4 36.8s-24.5 103.8-29 123.2c-5 21.4-3 51.6-.9 71.2C65.4 450.9 0 361.1 0 256 0 119 111 8 248 8s248 111 248 248z"></path>
-                  </svg>
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <>
+      <Layout className="layout-default layout-signin">
+        <Header>
+          <div className="header-col header-brand">
+            <h5>And Then Some 대시보드</h5>
+          </div>
+          <div className="header-col header-nav">
+            <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
+              <Menu.Item key="1">
+                <Link to="/dashboard">
+                  {template}
+                  <span> 대시보드</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item>
-                <Link to="#">{<GithubOutlined />}</Link>
+              <Menu.Item key="2">
+                <Link to="/profile">
+                  {profile}
+                  <span>프로필</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Link to="/sign-up">
+                  {signup}
+                  <span> 회원가입</span>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Link to="/sign-in">
+                  {signin}
+                  <span> 로그인</span>
+                </Link>
               </Menu.Item>
             </Menu>
-            <p className="copyright">
-              {" "}
-              Copyright © 2021 And Then Some by <a href="#pablo">정인용</a>.{" "}
-            </p>
-          </Footer>
-        </Layout>
-      </>
-    );
-  
+          </div>
+          <div className="header-col header-btn">
+            <Button type="primary">무료 다운로드</Button>
+          </div>
+        </Header>
+        <Content className="signin">
+          <Row gutter={[24, 0]} justify="space-around">
+            <Col
+              xs={{ span: 24, offset: 0 }}
+              lg={{ span: 6, offset: 2 }}
+              md={{ span: 12 }}
+            >
+              <Title className="mb-15">로그인</Title>
+              <Title className="font-regular text-muted" level={5}>
+                이메일과 비밀번호를 입력하세요.
+              </Title>
+              <Form
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                layout="vertical"
+                className="row-col"
+              >
+                <Form.Item
+                  className="username"
+                  label="이메일"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "이메일을 입력하세요!",
+                    },
+                  ]}
+                >
+                  <Input placeholder="이메일" />
+                </Form.Item>
+
+                <Form.Item
+                  className="username"
+                  label="비밀번호"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "비밀번호를 입력하세요!",
+                    },
+                  ]}
+                >
+                  <Input placeholder="비밀번호" type="password" />
+                </Form.Item>
+
+                <Form.Item
+                  // name="remember"
+                  className="aligin-center"
+                  valuePropName="checked"
+                >
+                  <Switch defaultChecked onChange={onChange} />
+                  로그인 유지
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ width: "100%" }}
+                  >
+                    로그인
+                  </Button>
+                </Form.Item>
+                <p className="font-semibold text-muted">
+                  계정이 없으신가요?&nbsp;{" "}
+                  <Link to="/sign-up" className="text-dark font-bold">
+                    회원가입
+                  </Link>
+                </p>
+              </Form>
+            </Col>
+            <Col
+              className="sign-img"
+              style={{ padding: 12 }}
+              xs={{ span: 24 }}
+              lg={{ span: 12 }}
+              md={{ span: 12 }}
+            >
+              <img src={signinbg} alt="" />
+            </Col>
+          </Row>
+        </Content>
+        <Footer>
+          <Menu mode="horizontal">
+            <Menu.Item>회사</Menu.Item>
+            <Menu.Item>가치</Menu.Item>
+            <Menu.Item>팀소개</Menu.Item>
+            <Menu.Item>제품</Menu.Item>
+            <Menu.Item>블로그</Menu.Item>
+            <Menu.Item>가격정책</Menu.Item>
+          </Menu>
+          <Menu mode="horizontal" className="menu-nav-social">
+            <Menu.Item>
+              <Link to="#">{<DribbbleOutlined />}</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="#">{<TwitterOutlined />}</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="#">{<InstagramOutlined />}</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="#">
+                <svg
+                  width="18"
+                  height="18"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M496 256c0 137-111 248-248 248-25.6 0-50.2-3.9-73.4-11.1 10.1-16.5 25.2-43.5 30.8-65 3-11.6 15.4-59 15.4-59 8.1 15.4 31.7 28.5 56.8 28.5 74.8 0 128.7-68.8 128.7-154.3 0-81.9-66.9-143.2-152.9-143.2-107 0-163.9 71.8-163.9 150.1 0 36.4 19.4 81.7 50.3 96.1 4.7 2.2 7.2 1.2 8.3-3.3.8-3.4 5-20.3 6.9-28.1.6-2.5.3-4.7-1.7-7.1-10.1-12.5-18.3-35.3-18.3-56.6 0-54.7 41.4-107.6 112-107.6 60.9 0 103.6 41.5 103.6 100.9 0 67.1-33.9 113.6-78 113.6-24.3 0-42.6-20.1-36.7-44.8 7-29.5 20.5-61.3 20.5-82.6 0-19-10.2-34.9-31.4-34.9-24.9 0-44.9 25.7-44.9 60.2 0 22 7.4 36.8 7.4 36.8s-24.5 103.8-29 123.2c-5 21.4-3 51.6-.9 71.2C65.4 450.9 0 361.1 0 256 0 119 111 8 248 8s248 111 248 248z"></path>
+                </svg>
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="#">{<GithubOutlined />}</Link>
+            </Menu.Item>
+          </Menu>
+          <p className="copyright">
+            {" "}
+            Copyright © 2021 And Then Some by <a href="#pablo">정인용</a>.{" "}
+          </p>
+        </Footer>
+      </Layout>
+    </>
+  );
 }
